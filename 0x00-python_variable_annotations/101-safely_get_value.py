f0831ae3cc5b269873bc32826c20ad79dacd5bf9
Chigozirim <iamokwukwe@gmail.com>
2022-06-23 20:46:49 +0100

    Do advanced task

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Defines the function safely_get_value."""
+from typing import TypeVar, Mapping, Any, Union
+
+
+T = TypeVar("T")
+
+
+def safely_get_value(
+    dct: Mapping, key: Any, default: Union[T, None] = None
+) -> Union[Any, T]:
+    """Returns the key of a dict if it exists."""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
