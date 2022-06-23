93356800eb814104f8bc77d3a1ff201767630959
Chigozirim <iamokwukwe@gmail.com>
2022-06-23 14:07:46 +0100

    Initial commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines the function add."""
+
+
+def add(a: float, b: float) -> float:
+    """Returns the sum of two floats."""
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines the function concat."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns concatenated strings."""
+    return str1 + str2
