6b8d98949b88a89855d44c117d7bee40743b5184
Chigozirim <iamokwukwe@gmail.com>
2022-06-23 20:22:00 +0100

    Align code to PEP8 standard

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -6,6 +6,7 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Returns a function that multiplies a float by multiplier."""
+
     def product(f: float):
         return f * multiplier
 
