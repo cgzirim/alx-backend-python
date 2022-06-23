5555d055feeb86fba2476438685be97c5fb33b10
Chigozirim <iamokwukwe@gmail.com>
2022-06-23 20:33:30 +0100

    Do advanced

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Defines the function safe_first_element."""
-from types import NoneType
 from typing import Union, Any, Sequence
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Returns the first element of a sequence if it exists."""
     if lst:
         return lst[0]
     else:
