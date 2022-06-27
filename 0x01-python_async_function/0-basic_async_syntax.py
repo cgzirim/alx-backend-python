616a342e4ef69dcb2c0fcea032fbf837ed832786
Chigozirim <iamokwukwe@gmail.com>
2022-06-27 16:00:40 +0100

    Do task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Defines the asynchronous coroutine wait_random."""
+import asyncio
+import random
+
+async def wait_random(max_delay=10):
+    """Waits for a random delay between 0 and max_delay seconds before
+    returning the random value.
+    """
+    random_int = random.uniform(0, max_delay)
+    await asyncio.sleep(random_int)
+    return random_int
