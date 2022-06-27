05908062ab900c06459ccb91d2f075398e91be6a
Chigozirim <iamokwukwe@gmail.com>
2022-06-27 16:01:11 +0100

    Do task 2

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Defines the function measure_time."""
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n, maxdelay):
+    """Measures the total execution time for wait_n(n, max_delay)."""
+    start_time = time.perf_counter()
+    wait_n(n, maxdelay)
+    return time.perf_counter() - start_time
