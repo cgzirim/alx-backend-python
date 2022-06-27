7e8c4a7981124356ed9d0372945f9b8664e9edc5
Chigozirim <iamokwukwe@gmail.com>
2022-06-27 16:40:41 +0100

    Do task

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,6 +2,8 @@
 """Defines the asynchronous coroutine wait_n()."""
 import asyncio
 from typing import List
+
+
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 """Defines the function measure_time."""
 import time
+from asyncio import run
+
+
 wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
-def measure_time(n: int, maxdelay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """Measures the total execution time for wait_n(n, max_delay)."""
     start_time = time.perf_counter()
-    wait_n(n, maxdelay)
+    run(wait_n(n, max_delay))
     return time.perf_counter() - start_time
