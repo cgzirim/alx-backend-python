eb2f8c7ee15bcb090fec489f15f12abef3c44507
Chigozirim <iamokwukwe@gmail.com>
2022-06-27 16:26:44 +0100

    Do task

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,6 +5,8 @@ from typing import List
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List(float):
-    """Spawn wait_random n times with the specified max_delay."""
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawn wait_random n times with the specified max_delay.
+    Return list of delay values.
+    """
     return [await wait_random(max_delay) for i in range(n)]
