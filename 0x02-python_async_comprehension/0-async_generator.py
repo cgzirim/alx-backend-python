fb71d9ae269e40a579c1c241d152d6d0aba59618
Chigozirim <iamokwukwe@gmail.com>
2022-06-29 12:26:29 +0100

    Do task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,4 +9,4 @@ async def async_generator() -> Generator[float, None, None]:
     """Yeilds a random number between 0 and 10. Delays for a second"""
     for i in range(10):
         yield random.uniform(0, 10)
-        await asyncio.sleep(1.0)
+        await asyncio.sleep(1)
