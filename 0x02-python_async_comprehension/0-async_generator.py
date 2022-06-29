ec4279fa43b7d9a7dc65fac800863d805cfa6785
Chigozirim <iamokwukwe@gmail.com>
2022-06-29 12:24:47 +0100

    Correct return type

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Yeilds a random number between 0 and 10. Delays for a second"""
     for i in range(10):
         yield random.uniform(0, 10)
