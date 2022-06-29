20858455d2a1bf8236f2432ef37d02c86de743e0
Chigozirim <iamokwukwe@gmail.com>
2022-06-29 13:04:54 +0100

    Do task 2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Defines a coroutine called measure_runtime."""
+import time
+import asyncio
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Runs async_comprehension 4 times and return the total runtime."""
+    start_time = time.perf_counter()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+    )
+
+    return time.perf_counter() - start_time
