85c02afd91053f79809385e2f9d2637952174277
Chigozirim <iamokwukwe@gmail.com>
2022-07-17 19:49:36 +0100

    Write unittest for utils.access_nested_map

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/pyhton3
+"""Defines a test class for util.access_nested_map."""
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test the function access_nested_map"""
+
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test access_nested_map function returns the expected result."""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([({}, ("a",), "a"), ({"a": 1}, ("a", "b"), "b")])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """Test access_neste_map raises KeyError for none existing keys."""
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+        self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
