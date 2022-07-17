b1363c8e5d8338c938ba39eee63011cc3613cf79
Chigozirim <iamokwukwe@gmail.com>
2022-07-17 21:37:57 +0100

    Test utils.get_json

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-"""Defines a test class for util.access_nested_map."""
+"""Defines test classes to test fuctions in ./utils."""
+from ast import expr_context
 import unittest
+from unittest.mock import patch
+
+from defer import return_value
+from utils import get_json
 from utils import access_nested_map
 from parameterized import parameterized
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Test the function access_nested_map"""
+    """Unittest for the function utils.access_nested_map"""
 
     @parameterized.expand(
         [
@@ -30,3 +35,19 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
         self.assertEqual(repr(e.exception), expected)
+
+
+class TestGetJson(unittest.TestCase):
+    """Unittest for the fuction utils.get_json."""
+
+    @parameterized.expand([
+            ('http://example.com', {"payload": True}),
+            ('http://holberton.io', {"payload": False})
+        ])
+    @patch("client.get_json")
+    def test_get_json(self, url, expected, mock_get_json):
+        """Tests that utils.get_json returns the expected result."""
+        mock_get_json.return_value = expected
+        self.assertEqual(mock_get_json(url), expected)
+        assert get_json(url) == expected
+        mock_get_json.assert_called_once()
